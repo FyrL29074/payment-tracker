c62a668a91c52bf404afc8a83ef5a7360412b606
Fyrl29074 <Fyrl29074@gmail.com>
2023-11-29 15:48:21 +0300

    Add StorageRepository

---
+++ app/src/main/java/com/fyrl29074/payment_tracker/data/repository/StorageRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.fyrl29074.payment_tracker.data.repository
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.fyrl29074.payment_tracker.domain.repository.StorageRepository
+
+class StorageRepositoryImpl(context: Context): StorageRepository {
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    override fun saveToken(token: String) {
+        sharedPreferences.edit().putString(TOKEN_KEY, token).apply()
+    }
+
+    override fun getToken(): String {
+        return sharedPreferences.getString(TOKEN_KEY, DEFAULT_TOKEN) ?: ""
+    }
+
+    companion object {
+        private const val PREFS_NAME = "AppPreferences"
+        private const val TOKEN_KEY = "token"
+        private const val DEFAULT_TOKEN = ""
+    }
+}
